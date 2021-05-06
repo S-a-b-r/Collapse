f9e8c2fb0fb508d0c79b272895368a341619f9ad
Sabr <57406194+S-a-b-r@users.noreply.github.com>
2021-05-06 19:15:51 +0400

    oh that's looks like somthing AMAZING)))0)

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import random
 N = 20
 matrix = []
 outMatrix = []
+countS = 0
+countG = 0
+countL = 0
 
 for i in range(N):
     matrix.append([])
@@ -17,11 +20,13 @@ def printMatrix():
     for i in range(N):
         for j in range(N):
             if(outMatrix[i][j] == 'G'):
-                out += colored(str(outMatrix[i][j]),'green')+" "
+                out += colored('G','green')+" "
+            #elif(outMatrix[i][j] == '0'):
+            #    out += colored('G','green')+" "
             elif(outMatrix[i][j] == 'S'):
-                out += colored(str(outMatrix[i][j]),'cyan') + " "
+                out += colored('S','cyan') + " "
             elif(outMatrix[i][j] == 'L'):
-                out += colored(str(outMatrix[i][j]),'yellow') + " "
+                out += colored('L','yellow') + " "
             else:
                 out += colored(str(outMatrix[i][j]),'red')+ " "
             #out += str(outMatrix[i][j])+ " "
@@ -42,6 +47,7 @@ def findNextTile():
     global countItaration
     if(countItaration == 0):
         countItaration+=1
+        #return int(N/2),0
         return random.randint(int(N/3), int(N-N/3)),random.randint(int(N/3), int(N-N/3))
     minVarTile = 4
     minI = 0
@@ -54,17 +60,48 @@ def findNextTile():
                 minJ = j
     return minI,minJ
 
-def updateOutMatrix():
-    for i in range(N):
-        for j in range(N):
-            if(len(matrix[i][j])==1):
-                outMatrix[i][j] = matrix[i][j][0]
-                matrix[i][j].pop()
+#def updateOutMatrix():
+#    for i in range(N):
+#        for j in range(N):
+#            if(len(matrix[i][j])==1):
+#                outMatrix[i][j] = matrix[i][j][0]
+#                matrix[i][j].pop()
+
+
 
 def iteration():
+
+    global countS
+    global countG
+    global countL
+
     i,j = findNextTile()
     try:
-        outMatrix[i][j] = matrix[i][j][random.randint(0,len(matrix[i][j])-1)]
+        #if(len(matrix[i][j]) == 3):
+        #    outMatrix[i][j] = random.choices(matrix[i][j], weights=[100,20,100])[0]
+        #elif(len(matrix[i][j])== 1):
+        #    outMatrix[i][j] == matrix[i][j][0][0]
+        #elif('L' in matrix[i][j] and 'G' in matrix[i][j]):
+        #    outMatrix[i][j] = random.choices(matrix[i][j], weights=[3,1000])[0]
+        #elif('S' in matrix[i][j] and 'G' in matrix[i][j]):
+        #    outMatrix[i][j] = random.choices(matrix[i][j], weights=[1000,3])[0]
+
+        #w = 0
+        #arr = []
+        #for i in range(len(matrix[i][j])):
+        #    arr.append(w)
+        #    w+=1
+        
+        if(len(matrix[i][j]) == 3):
+            outMatrix[i][j] = matrix[i][j][random.choices([0,1,2],weights = [40,20,40])[0]]
+        elif(len(matrix[i][j])==2 and 'S' in matrix[i][j]):
+            outMatrix[i][j] = matrix[i][j][random.choices([0,1],weights = [500,20])[0]]
+        elif(len(matrix[i][j])==2 and 'L' in matrix[i][j]):
+            outMatrix[i][j] = matrix[i][j][random.choices([0,1],weights = [20,40])[0]]
+        elif(len(matrix[i][j])==1):
+            outMatrix[i][j] = matrix[i][j][0]
+        #outMatrix[i][j] = matrix[i][j][random.choices(arr,weights = [40,20,40])[0]]
+        #outMatrix[i][j] = matrix[i][j][random.randint(0,len(matrix[i][j])-1)]
     except:
         pass
     #Чистим активный элемент начальной матрицы
@@ -74,6 +111,7 @@ def iteration():
     #Чистим остальные элементы матрицы
     #Если активный элемент стал Небом
     if(outMatrix[i][j] == 'S'):
+        countS+=1
 
         try:
             if(i-1>=0):
@@ -105,7 +143,8 @@ def iteration():
     
     #Если активный элемент стал травой
     if(outMatrix[i][j] == 'G'):
-
+        countG+=1
+        
         try:
             if(i-1>=0):
                 matrix[i-1][j].remove('G')
@@ -118,6 +157,12 @@ def iteration():
         except:
             pass
 
+        #try:
+        #    if(i-1>=0):
+        #        matrix[i+1][j].remove('G')
+        #except:
+        #    pass
+
         try:
             matrix[i+1][j].remove('S')
         except:
@@ -126,6 +171,7 @@ def iteration():
     
     #Если активный элемент стал Землей
     if(outMatrix[i][j] == 'L'):
+        countL+=1
 
         try:
             if(i-1>=0):
@@ -161,5 +207,7 @@ def iteration():
 for i in range(N):
     for j in range(N):
         iteration()
-
+arr = [0,1,2]
+#print(matrix[0][0][random.choices(arr,weights = [40,20,40])[0]])
 printMatrix()
+#printMatrix1()
